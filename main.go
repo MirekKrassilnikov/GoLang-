9b43f3fd29252c63ba7fa1e25557ab2f2cdbb46a
MirekKrassilnikov <mirekrassilnikov@gmail.com>
2023-11-19 16:15:57 +0100

    Rows and columns to check for master Andrew

--- main.go
+++ main.go
@@ -7,22 +7,6 @@ import (
 
 func main() {
 
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	/*
 	//SLICE CREATE
 	var columns int
 	var rows int
@@ -47,6 +31,97 @@ func main() {
 		}
 	}
 
+	for _, value := range multiSlice {
+		fmt.Println(value)
+	}
+	//MAX NUMBER
+	maxNumberRow := multiSlice[0][0]
+	for k, _ := range multiSlice {
+		for _, val := range multiSlice[k] {
+			if val > maxNumberRow {
+				maxNumberRow = val
+
+			}
+
+		}
+	}
+	fmt.Println("Highest number is ", fmt.Sprint(maxNumberRow))
+	//MIN NUMBER
+	minNumberRow := multiSlice[0][0]
+	for k, _ := range multiSlice {
+		for _, val := range multiSlice[k] {
+			if val < minNumberRow {
+				minNumberRow = val
+
+			}
+
+		}
+	}
+	fmt.Println("Lowest number is ", fmt.Sprint(minNumberRow))
+	//ALL ROWS SUMARIZE
+	for i, value := range multiSlice {
+		sum := 0
+		for _, val := range value {
+			sum = sum + val
+		}
+		fmt.Println("Summary of the row num.", fmt.Sprint(i+1), "is", fmt.Sprint(sum))
+	}
+
+	//ALL COLUMNS SUMMARIZE
+	for i, value := range multiSlice {
+		sum := 0
+		for n, _ := range value {
+			sum = sum + multiSlice[n][i]
+		}
+		fmt.Println("Summary of the column num.", fmt.Sprint(i+1), "is", fmt.Sprint(sum))
+	}
+	//EXACT ROW SUMMARIZE
+	fmt.Println("Enter the row you want to summarize")
+
+	var rowNum int
+	var sum int
+	fmt.Scan(&rowNum)
+	for _, value := range multiSlice[rowNum-1] {
+		sum = sum + value
+	}
+	fmt.Printf("Summary of the row num. %d is %d", rowNum, sum)
+
+	//EXACT COLUMN SUMMARIZE
+	fmt.Println("\n Enter the column you want to summarize")
+
+	var colNum int
+	var colSum int
+	fmt.Scan(&colNum)
+	for i, _ := range multiSlice {
+		colSum = colSum + multiSlice[i][colNum-1]
+	}
+	fmt.Printf("Summary of the col num. %d is %d", rowNum, colSum)
+
+	//SORT LOW TO HIGH ROWS
+
+	for _, value := range multiSlice {
+		sortedSlice := []int{}
+		for len(value) > 0 {
+			lowestNumber := value[0]
+			lowestIndex := 0
+
+			for i, val := range value {
+				if val < lowestNumber {
+					lowestNumber = val
+					lowestIndex = i
+				}
+			}
+
+			sortedSlice = append(sortedSlice, lowestNumber)
+
+			value = append(value[:lowestIndex], value[lowestIndex+1:]...)
+		}
+
+		fmt.Println(sortedSlice)
+
+	}
+
+	//SORT COLUMNS LOW TO HIGH
 	for k := 0; k < rows; k++ {
 
 		for f := 0; f < rows; f++ {
@@ -62,92 +137,8 @@ func main() {
 		}
 
 	}
+	fmt.Println("Columns sorted low to high")
 	for _, value := range multiSlice {
-		fmt.Println(value) /*
-			//MAX NUMBER
-			maxNumberRow := multiSlice[0][0]
-			for k, _ := range multiSlice {
-				for _, val := range multiSlice[k] {
-					if val > maxNumberRow {
-						maxNumberRow = val
-
-					}
-
-				}
-			}
-			fmt.Println("Highest number is ", fmt.Sprint(maxNumberRow))
-			//MIN NUMBER
-			minNumberRow := multiSlice[0][0]
-			for k, _ := range multiSlice {
-				for _, val := range multiSlice[k] {
-					if val < minNumberRow {
-						minNumberRow = val
-
-					}
-
-				}
-			}
-			fmt.Println("Lowest number is ", fmt.Sprint(minNumberRow))
-			//ALL ROWS SUMARIZE
-			for i, value := range multiSlice {
-				sum := 0
-				for _, val := range value {
-					sum = sum + val
-				}
-				fmt.Println("Summary of the row num.", fmt.Sprint(i+1), "is", fmt.Sprint(sum))
-			}
-
-			//ALL COLUMNS SUMMARIZE
-			for i, value := range multiSlice {
-				sum := 0
-				for n, _ := range value {
-					sum = sum + multiSlice[n][i]
-				}
-				fmt.Println("Summary of the column num.", fmt.Sprint(i+1), "is", fmt.Sprint(sum))
-			}
-			//EXACT ROW SUMMARIZE
-			fmt.Println("Enter the row you want to summarize")
-
-			var rowNum int
-			var sum int
-			fmt.Scan(&rowNum)
-			for _, value := range multiSlice[rowNum-1] {
-				sum = sum + value
-			}
-			fmt.Printf("Summary of the row num. %d is %d", rowNum, sum)
-
-			//EXACT COLUMN SUMMARIZE
-			fmt.Println("\n Enter the column you want to summarize")
-
-			var colNum int
-			var colSum int
-			fmt.Scan(&colNum)
-			for i, _ := range multiSlice {
-				colSum = colSum + multiSlice[i][colNum-1]
-			}
-			fmt.Printf("Summary of the col num. %d is %d", rowNum, colSum)
-
-			//SORT LOW TO HIGH ROWS
-
-			for _, value := range multiSlice {
-				sortedSlice := []int{}
-				for len(value) > 0 {
-					lowestNumber := value[0]
-					lowestIndex := 0
-
-					for i, val := range value {
-						if val < lowestNumber {
-							lowestNumber = val
-							lowestIndex = i
-						}
-					}
-
-					sortedSlice = append(sortedSlice, lowestNumber)
-
-					value = append(value[:lowestIndex], value[lowestIndex+1:]...)
-				}
-
-				fmt.Println(sortedSlice)
-
-			}
-		*/
+		fmt.Println(value)
+	}
+}
